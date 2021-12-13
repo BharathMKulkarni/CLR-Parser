200f86359fce384480b7cce6173892910d03b22c
BharathMKulkarni <bharathmkulkarni@gmail.com>
2021-12-13 07:06:32 +0530

    added support to detect any valid variable name

--- lexical_analyzer.py
+++ lexical_analyzer.py
@@ -1,6 +1,7 @@
 import re
 regex_digit = '^[0-9]+$'
 regex_float = '[+-]?([0-9]*[.])?[0-9]+[E-]?[E+]?[0-9]'
+regex_names = '^[a-zA-Z_$][a-zA-Z_$0-9]*$'
 SYMBOLS = ['(',
            ')',
            ';',
@@ -50,12 +51,13 @@ def generate_tokens(filename):
                 tokens.append('o')
             elif word in SYMBOLS:
                 tokens.append(word)
-            else:
+            elif(re.search(regex_names,word)):
                 tokens.append('v')
+                continue
 
     token_string = "".join(tokens)
     
     return token_string
 
 
-generate_tokens("code5.txt")
+generate_tokens("input_code.txt")
